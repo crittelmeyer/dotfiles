b24ebad380b4905492768298db22c93d408552fd
Chris Rittelmeyer <chrisrittelmeyer@gmail.com>
2016-05-13 15:47:48 -0400

    Add installation of gpg (required for rvm)

--- brew.sh
+++ brew.sh
@@ -61,6 +61,7 @@ brew install phantomjs
 brew install node
 
 # Install rvm
+brew install gpg
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
