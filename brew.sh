003ec79493269d56d9560976ff82d820fbbfc911
Chris Rittelmeyer <chrisrittelmeyer@gmail.com>
2016-05-13 16:51:42 -0400

    Move rvm script sourcing up a few steps

--- brew.sh
+++ brew.sh
@@ -67,9 +67,9 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 
 # ruby versions
 rvm get stable --auto-dotfiles
+source /usr/local/rvm/scripts/rvm
 rvm install 2.0.0-p645
 rvm install 2.3.0
-source /usr/local/rvm/scripts/rvm
 rvm use 2.3.0
 
 # gems
