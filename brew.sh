5a413d30636ee4117a4879d5494680034947d29a
Chris Rittelmeyer <chrisrittelmeyer@Users-MacBook-Pro-2.local>
2022-01-05 21:09:03 -0500

    update brew.sh

--- brew.sh
+++ brew.sh
@@ -3,22 +3,19 @@
 # Install command-line tools using Homebrew.
 # Mostly stolen from https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -42,70 +39,42 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install `wget`
+brew install wget
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
 
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
 brew install ctags
 
-# Install development tools.
-brew install python
-brew install ghc cabal-install
-brew install phantomjs
-brew install node
-
-# Install rvm
-brew install gpg
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
-# ruby versions
-rvm get stable --auto-dotfiles
-source /usr/local/rvm/scripts/rvm
-rvm install 2.0.0-p645
-rvm install 2.3.0
-rvm use 2.3.0
-
-# gems
-gem install bundler
-gem install rails
-gem install pry
-gem install byebug
-gem install unicorn
-gem install rspec
-gem install guard
-gem install teaspoon-mocha
-gem install rubocop
-gem install scss_lint
-gem install haml_lint
-
-# pip modules
-pip install flake8
-pip install git+git://github.com/Lokaltog/powerline
-
 # node modules
-npm install -g bower
-npm install -g cypress-cli
-npm install -g redux-cli
 npm install -g deployd
 npm install -g eslint
-npm install -g mocha
-npm install -g npmrc
-npm install -g n_
-npm install -g jest
 
 # Install other useful binaries.
 brew install zsh
 brew install ack
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
+brew install imagemagick
 brew install jp2a
 brew install lynx
 brew install p7zip
